59254e423bc7d8e52631ef93431196c5ab651290
TheRealAmplifier <coding@jeroenpielage.nl>
2018-05-21 22:13:48 +0200

    Added basic form for registration (needs a lot of work still)

--- imports/ui/signup.js
+++ imports/ui/signup.js
@@ -2,11 +2,38 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 class SignUp extends Component {
-	render() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			error: ''
+		}
+	}
+	onFormSubmit(event) {
+		event.preventDefault();
+
+		this.setState({
+			error: 'Something went wrong'
+		});
+	}
+ 	render() {
 		return (
 			<div>
 				<h1>Sign Up</h1>
-				{/* login form */}
+
+				{ this.state.error ? <p>{this.state.error}</p> : undefined }
+
+				<form onSubmit={this.onFormSubmit.bind(this)}>
+					<fieldset>
+						<label for="email">Email address</label>
+						<input type="email" name="email" />
+					</fieldset>
+					<fieldset>
+						<label for="email">Password</label>
+						<input type="password" name="password" />
+					</fieldset>
+					<button>Create account</button>
+				</form>
+
 				<Link to="/login">Login</Link>
 			</div>
 		);
